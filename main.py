8fdc6f4928bf578c9292dd8efb698902c6fe1819
Ruanqin <teldrassildruid@gmail.com>
2021-12-27 16:51:37 -0500

    add ball class and ball bouncing logic

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
+COLLISION_DIS = 15
+CEILING_YCOR = 280
+FLOOR_YCOR = -280
+PADDLE_DIS_MAX = 50
+PADDLE_HIT_XCOR_R = 320
+PADDLE_HIT_XCOR_L = -320
 
 screen = Screen()
 paddle_right = Paddle(350)
 paddle_left = Paddle(-350)
+ball = Ball()
 
 screen.title('My Pong Game')
 screen.bgcolor('black')
@@ -18,6 +28,17 @@ screen.onkey(paddle_left.paddle_down, 's')
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with ceiling and floor
+    if ball.ycor() > CEILING_YCOR or ball.ycor() < FLOOR_YCOR:
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if ball.distance(paddle_right) < PADDLE_DIS_MAX and ball.xcor() > PADDLE_HIT_XCOR_R or ball.distance(paddle_left) and ball.xcor() < PADDLE_HIT_XCOR_L:
+        ball.bounce_x()
+
 
 screen.exitonclick()
